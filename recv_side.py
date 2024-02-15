4f9981512d093424814ba91528790dab479a66a1
Kyuho Kim <ekyuho@gmail.com>
2024-02-15 12:54:36 +0900

    Add files via upload

---
+++ recv_side.py
@@ -0,0 +1,67 @@
+import random
+import time
+import json
+from paho.mqtt import client as mqtt_client
+
+broker = 'splavice.io'
+port = 1883
+client_id = f'fire-alarm-recv-{random.randint(0, 1000)}'
+
+flag_connected = 0
+TOPIC="splavice/fire/data/#"
+file_path = 'recv_data.csv'
+n=1
+
+with open(file_path, 'w') as file:
+    out = 'topic,temp,co2,ir,date,time,fire-score'
+    file.write(out + '\n')
+
+def do_line(topic, payload):
+    with open(file_path, 'a') as file:
+        j = json.loads(payload)
+        print(n, j)
+        out = f'''{topic},{j['temp']},{j['co2']},{j['ir']},{j['time'].split(' ')[0]},{j['time'].split(' ')[1]},{j['fire-score']}'''
+        # Write the data to the file
+        file.write(out + '\n')
+        file.flush()  # Flush the buffer to ensure data is written ij.ediately
+
+        # Print the data to the console (optional)
+        print(n, out)
+
+
+
+def on_message(client, _t, msg):
+    global n
+    #if topic.startswith('s2m') and '/data' not in topic and not topic.endswith('stat'):
+    do_line(msg.topic, msg.payload)
+    n+=1
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker", broker)
+            global flag_connected
+            flag_connected = 1
+            if TOPIC != "":
+                client.subscribe(TOPIC)
+                print("subscribed to {}".format(TOPIC))
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    def on_disconnect(client, userdata, rc):
+        global flag_connected
+        flag_connected = 0
+        print("Disconnected from MQTT Broker!")
+
+
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
+    if flag_connected == 0:
+        client.connect(broker, port)
+    return client
+
+client = connect_mqtt()
+client.loop_forever()
---
+++ send_side.py
@@ -0,0 +1,54 @@
+import serial
+import re
+import json
+import sys
+
+# Define the serial port settings
+port = 'COM9'  # Change this to match your serial port
+baud_rate = 115200  # Change this to match your baud rate
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 send_side.py COMX")
+        return
+
+    # Open the serial port
+    ser = serial.Serial(port, baud_rate)
+
+    # Open a file to save the data
+    file_path = 'send_data.csv'
+    n=1
+    with open(file_path, 'w') as file:
+        out = 'topic,temp,co2,ir,date,time,fire-score'
+        print(' ', out)
+        file.write(out + '\n')
+        try:
+            while True:
+                # Read a line of data from the serial port
+                line = ser.readline().decode().strip()
+                #print(line)
+                if not line.startswith('splavice'):
+                    print(f'skip {line}')
+                    continue
+                    
+                i=line.find('{')
+                input= line[i:]
+                topic=line[:i-1]
+                #print(input)
+                j = json.loads(input)
+                #print(n, j)
+                out = f'''{topic},{j['temp']},{j['co2']},{j['ir']},{j['time'].split(' ')[0]},{j['time'].split(' ')[1]},{j['fire-score']}'''
+                # Write the data to the file
+                file.write(out + '\n')
+                file.flush()  # Flush the buffer to ensure data is written ij.ediately
+
+                # Print the data to the console (optional)
+                print(n, out)
+                n+=1
+
+        except KeyboardInterrupt:
+            print("Keyboard interrupt detected. Closing the serial port.")
+            ser.close()
+        
+if __name__ == "__main__":
+    main()
